59de7f97767cab9d175101247a1367c8010ebdb9
robertomorano <roberto.morano@iesnervion.es>
2025-03-06 10:01:18 +0100

    conflictos resuelto

--- src/fecha/Fecha.java
+++ src/fecha/Fecha.java
@@ -6,7 +6,6 @@ package fecha;
  * @author anonimo
  */
 public class Fecha {
-<<<<<<< HEAD
 	/**
 	 * Atributo del dia para la fecha
 	 */
@@ -19,14 +18,8 @@ public class Fecha {
 	 * Atributo del dia para la fecha
 	 */
 	private int año; // a�o
-=======
-	public static final int DIEZ = 10;
-	public static final int DIEZ = DIEZ;
-	private int d; //d�a
-	private int m; //mes
-	private int a; //a�o
->>>>>>> PMD-Refactor
 
+	public static final int DIEZ = 10;
 	/**
 	 * Constructor vacio
 	 */
@@ -53,75 +46,53 @@ public class Fecha {
 	 * @return un booleano para ver si la fecha es correcta
 	 */
 	public boolean fechaCorrecta() {
-<<<<<<< HEAD
 		boolean diaCorrecto; 
 		boolean mesCorrecto;
 		boolean anioCorrecto;
 		anioCorrecto = año > 0;
 		mesCorrecto = month >= 1 && month <= 12;
+		boolean diaMayor1 = dia >= 1;
 		switch (month) {
 		case 2:
 			if (esBisiesto()) {
-				diaCorrecto = dia >= 1 && dia <= 29;
+				diaCorrecto = diaMayor1 && dia <= 29;
 			} else {
-				diaCorrecto = dia >= 1 && dia <= 28;
-=======
-		boolean diaCorrecto, mesCorrecto, anioCorrecto;
-		anioCorrecto = a > 0;
-		mesCorrecto = m >= 1 && m <= 12;
-		boolean diaMayor1 = d >= 1;
-		switch (m) {
-		case 2:
-			if (esBisiesto()) {
-				diaCorrecto = diaMayor1 && d <= 29;
-			} else {
-				diaCorrecto = diaMayor1 && d <= 28;
->>>>>>> PMD-Refactor
+				diaCorrecto = diaMayor1 && dia <= 28;
 			}
 			break;
 		case 4:
 		case 6:
 		case 9:
 		case 11:
-<<<<<<< HEAD
-			diaCorrecto = dia >= 1 && dia <= 30;
+			diaCorrecto = diaMayor1 && dia <= 30;
 			break;
 		default:
-			diaCorrecto = dia >= 1 && dia <= 31;
-=======
-			diaCorrecto = diaMayor1 && d <= 30;
-			break;
-		default:
-			diaCorrecto = diaMayor1 && d <= 31;
->>>>>>> PMD-Refactor
+			diaCorrecto = diaMayor1 && dia <= 31;
 		}
 		return diaCorrecto && mesCorrecto && anioCorrecto;
 	}
 
 	// M�todo esBisiesto. Solo lo usa fechaCorrecta, por eso es privado
-<<<<<<< HEAD
+
 	/**
 	 * Metodo esBisiesto. Ciertos años tiene en febrero un dia mas, metodo inerno
 	 * 
 	 * @return si el año es bisiesto
 	 */
-=======
->>>>>>> PMD-Refactor
+
 	private boolean esBisiesto() {
 		return año % 4 == 0 && año % 100 != 0 || año % 400 == 0;
 	}
 
 	// M�todo diaSiguiente
-<<<<<<< HEAD
+
 	/**
 	 * Pasa de dia y compruba si se ha cambiado de mes
 	 */
-	public void diaSiguiente() {
-		dia++;
-=======
+
 	public void nextDay() {
-		d++;
->>>>>>> PMD-Refactor
+		dia++;
+
 		if (!fechaCorrecta()) {
 			dia = 1;
 			month++;
@@ -132,7 +103,7 @@ public class Fecha {
 		}
 	}
 
-<<<<<<< HEAD
+
 	/**
 	 * Metodo toString de la clase
 	 * 
@@ -146,16 +117,6 @@ public class Fecha {
 			texto = "0" + dia + "-" + month + "-" + año;
 		} else if (dia >= 10 && month < 10) {
 			texto = dia + "-0" + month + "-" + año;
-=======
-	// M�todo toString
-	public String toString() {
-		if (d < DIEZ && m < DIEZ) {
-			return "0" + d + "-0" + m + "-" + a;
-		} else if (d < DIEZ && m >= DIEZ) {
-			return "0" + d + "-" + m + "-" + a;
-		} else if (d >= DIEZ && m < DIEZ) {
-			return d + "-0" + m + "-" + a;
->>>>>>> PMD-Refactor
 		} else {
 			texto = dia + "-" + month + "-" + año;
 		}
